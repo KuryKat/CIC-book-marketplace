d98fe8d4c46dba876799a7064fb3a70b42713016
KuryKat <57152799+KuryKat@users.noreply.github.com>
2022-04-17 01:54:38 -0300

    Update and bugfix basic routes

--- src/routes/API/index.ts
+++ src/routes/API/index.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express'
 import booksRouter from './books'
+import usersRouter from './users'
 
 const router = Router()
 
 router.get('/', (_, res) => res.send({ message: 'Hey! The API starts here!' }))
-router.get('/books', booksRouter)
+router.use('/books', booksRouter)
+router.use('/users', usersRouter)
 
 export default router
