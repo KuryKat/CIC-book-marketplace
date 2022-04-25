4ff5d510128cb496a425a233dce5dd3bd3002080
KuryKat <57152799+KuryKat@users.noreply.github.com>
2022-04-25 06:50:24 -0300

    Update "somethingThatProcessThePurchase" function
    Update to use new HTTPError class to better handling on the other side

--- src/utils/somethingThatProcessThePurchase.ts
+++ src/utils/somethingThatProcessThePurchase.ts
@@ -1,20 +1,26 @@
 import { Book } from '@modules/database/schemas/Book.schema'
 import { User } from '@modules/database/schemas/User.schema'
 import UserService from '@modules/database/services/User.service'
+import HTTPError from './Errors/HTTPError'
 
 export default async function somethingThatProcessThePurchase (user: User, book: Book, userService: UserService): Promise<void> {
-  const chanceToFail = Boolean(Math.random() < 0.9)
+  const chanceToFail = Boolean(Math.random() < 0.45)
   if (chanceToFail) {
-    throw new Error('3032 | Sorry, the payment cannot be completed now.')
+    throw new HTTPError(402, 'Sorry, the payment cannot be completed now.')
   }
 
-  (user.details.purchasedBooks as string[]).push(book._id)
+  const userBooks = user.details.purchasedBooks as string[]
+  if (userBooks.includes(book._id)) {
+    throw new HTTPError(400, 'You already have this book! Try downloading it!')
+  }
+
+  userBooks.push(book._id)
   await userService.updateUser(user, user, 'auto')
 
   const seller = await userService.getUserByID((book.seller as User)._id)
 
   if (seller == null) {
-    throw new Error('404 | Seller Not Found')
+    throw new HTTPError(404, 'Seller Not Found')
   }
 
   seller.details.booksSold++
